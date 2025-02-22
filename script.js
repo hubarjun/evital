8397fc6c4ffbcd2a032ce2befdc556088c96dfec
arjun pandi <arjunpandi707@gmail.com>
2025-02-23 00:10:43 +0530

    add location

--- script.js
+++ script.js
@@ -17,18 +17,48 @@ function isAppleDevice() {
   AFRAME.registerComponent("mytarget", {
     init: function () {
       window.addEventListener("contextmenu",(e)=>e.preventDefault());
+      document.addEventListener('keydown', function(e) {
+        if (e.key === 'F12' || (e.ctrlKey && (e.key === 'u' || e.key === 'U'))) {
+          e.preventDefault();
+        }
+        
+        
+        
+        if (e.ctrlKey && e.shiftKey && e.key === 'C') {
+          e.preventDefault();
+        }
+      });
+      
       const showInfo = () =>
 		{
 			let y = 0;
 
 			const websiteButton = document.querySelector( "#website-btn" );
-      const locationButton = document.querySelector( "#location-btn" );
+      const locationButton1 = document.querySelector( "#location-btn1" );
+      const locationButton2 = document.querySelector( "#location-btn2" );
+      const locationButton3 = document.querySelector( "#location-btn3" );
+      const locationButton4 = document.querySelector( "#location-btn4" );
+      const locationButton5 = document.querySelector( "#location-btn5" );
+      const locationText1 = document.querySelector( "#locationText1" );
+      const locationText2 = document.querySelector( "#locationText2" );
+      const locationText3 = document.querySelector( "#locationText3" );
+      const locationText4 = document.querySelector( "#locationText4" );
+      const locationText5 = document.querySelector( "#locationText5" );
 
 			
 			
       setTimeout( () =>
 			{
-				locationButton.setAttribute( "visible", true );
+				locationButton1.setAttribute( "visible", true );
+				locationButton2.setAttribute( "visible", true );
+				locationButton3.setAttribute( "visible", true );
+				locationButton4.setAttribute( "visible", true );
+				locationButton5.setAttribute( "visible", true );
+				locationText1.setAttribute( "visible", true );
+				locationText2.setAttribute( "visible", true );
+				locationText3.setAttribute( "visible", true );
+				locationText4.setAttribute( "visible", true );
+				locationText5.setAttribute( "visible", true );
 			}, 500 );
       setTimeout( () =>
 			{
@@ -36,12 +66,50 @@ function isAppleDevice() {
 			}, 600 );
 
 		
-			locationButton.addEventListener( 'click', function ( evt )
+			locationButton1.addEventListener( 'click', function ( evt )
 			{
-				window.location.href = "";
+				window.location.href = "https://maps.app.goo.gl/kbCZ1r9W5pBwXAjr7";
+			} );
+			locationText1.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/kbCZ1r9W5pBwXAjr7";
+			} );
+			locationButton2.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/EXfm7fD11RTGF8M87";
+			} );
+			locationText2.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/EXfm7fD11RTGF8M87";
+			} );
+			locationButton3.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/GHvHWomY2bnwrVmx9";
+			} );
+			locationText3.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/GHvHWomY2bnwrVmx9";
+			} );
+			locationButton4.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/77ckkpMovtjLKRcaA";
+			} );
+			locationText4.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/77ckkpMovtjLKRcaA";
+			} );
+			locationButton5.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/Yo977YY9kYiRaBsV9";
+			} );
+			locationText5.addEventListener( 'click', function ( evt )
+			{
+				window.location.href = "https://maps.app.goo.gl/Yo977YY9kYiRaBsV9";
 			} );
 			websiteButton.addEventListener( 'click', function ( evt )
 			{
+        console.log("clicked");
+        
 				window.location.href = "service/service.html";
 			} );
 		}
